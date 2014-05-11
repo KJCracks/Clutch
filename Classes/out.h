e2a843175de5b2979bef8d48fdca10b581b672ba
Andrew Udvare <audvare@gmail.com>
2014-05-11 07:26:10 -0700

    Define CLUTCH_DEBUG conditionally (NDEBUG or set by user with xcodebuild)

--- Classes/out.h
+++ Classes/out.h
@@ -17,9 +17,11 @@
 
 #define NSPrint(M, ...) fprintf(stderr, "%s \n", [[NSString stringWithFormat:M, ##__VA_ARGS__] UTF8String]);
 
+#if !defined(CLUTCH_DEBUG) || !defined(NDEBUG)
 #define CLUTCH_DEBUG 1
+#endif
 
-#ifdef CLUTCH_DEBUG
+#if CLUTCH_DEBUG
 #   define FILE_NAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__) // shortened path of __FILE__ is there is one
 #
 #   define NSLog(M, ...) fprintf(stderr, "\033[0;32mDEBUG\033[0m | %s:%d | %s\n", FILE_NAME, __LINE__, [[NSString stringWithFormat:M, ##__VA_ARGS__] UTF8String]);

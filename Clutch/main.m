51b5c6530bb226efc008895b199cdccae127b9a5
Anton Titkov <atitkov@iMac-Anton.local>
2015-02-09 20:27:29 +0300

    Initial Commit

---
+++ Clutch/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  Clutch
+//
+//  Created by Anton Titkov on 09.02.15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool
+    {	
+    	// insert code here...
+    	NSLog(@"Hello, World!");	
+    }
+	return 0;
+}
+

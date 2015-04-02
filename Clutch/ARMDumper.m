9d6bee80f821796dfaffb65d9bac75283057cd1c
ttwj <terencetanwj@gmail.com>
2015-04-02 22:57:54 +0800

    stuff

--- Clutch/ARMDumper.m
+++ Clutch/ARMDumper.m
@@ -10,8 +10,10 @@
 #import <mach-o/fat.h>
 #import "Device.h"
 #import <dlfcn.h>
+#import <mach/mach.h>
 #import <mach/mach_traps.h>
 #import <mach/mach_init.h>
+#import <mach-o/dyld_images.h>
 
 @implementation ARMDumper
 
@@ -188,6 +190,143 @@
     }
     
     BOOL dumpResult = [self _dumpToFileHandle:newFileHandle withEncryptionInfoCommand:(crypt.cryptsize + crypt.cryptoff) pages:pages fromPort:port pid:pid aslrSlide:__text_start];
+    
+    //find dylibs swag
+    
+    task_dyld_info_data_t task_dyld_info;
+    mach_msg_type_number_t count = TASK_DYLD_INFO_COUNT;
+    
+    kern_return_t kr = task_info(self, TASK_DYLD_INFO ,(task_info_t)&task_dyld_info, &count);
+    if (kr != KERN_SUCCESS) {
+        DumperLog(@"Could not find dyld info!??");
+        return NO;
+    }
+    mach_vm_address_t addr = task_dyld_info.all_image_info_addr;
+    struct dyld_all_image_infos *info = (struct dyld_all_image_infos*) addr;
+
+    
+    printf("%d\n", info->version);
+    uint32_t ImageCount = info->infoArrayCount;
+    printf("%d\n", ImageCount);
+    
+    struct mach_header dylib_header;
+    
+    
+    for (int i = 0; i < ImageCount; ++i) {
+        struct dyld_image_info ImageInfo = info->infoArray[i];
+        uint32_t address = (uint32_t) ImageInfo.imageLoadAddress;
+        printf("%s %x\n", ImageInfo.imageFilePath, address);
+        
+        
+        BOOL dumpResult = [self _dumpToFileHandle:newFileHandle withEncryptionInfoCommand:(crypt.cryptsize + crypt.cryptoff) pages:pages fromPort:port pid:pid aslrSlide:__text_start];
+        
+        //todo: check if that framework exists
+   
+        struct load_command l_cmd; // generic load comman
+    
+        uint32_t _address = address + sizeof(struct mach_header);
+        
+        uint32_t framework_cryptsize;
+        mach_vm_size_t local_size = 0;
+        
+        for (int lc_index = 0; lc_index < ImageInfo.imageLoadAddress->ncmds; lc_index++) { // iterate over each load command
+            
+            err = mach_vm_read_overwrite(self, _address, sizeof (struct load_command), &l_cmd, &local_size);
+            if (err != KERN_SUCCESS) {
+                NSLog(@"failed to read load command");
+                return false;
+            }
+            else if (l_cmd.cmd == LC_ENCRYPTION_INFO) {
+                //find the cryptsize fuck yea
+                struct encryption_info_command crypt;
+                mach_vm_read_overwrite(self, _address, sizeof (struct encryption_info_command), &crypt, &local_size);
+                framework_cryptsize = crypt.cryptsize;
+                
+            }
+            _address += sizeof(struct segment_command);
+        }
+        
+        
+        
+        // perform checks on vm regions
+        memory_object_name_t object;
+        vm_region_basic_info_data_t info;
+        //mach_msg_type_number_t info_count = VM_REGION_BASIC_INFO_COUNT;
+        mach_msg_type_number_t info_count = VM_REGION_BASIC_INFO_COUNT;
+        mach_vm_address_t region_start = 0;
+        mach_vm_size_t region_size = 0;
+        vm_region_flavor_t flavor = VM_REGION_BASIC_INFO;
+        err = 0;
+        
+        mach_vm_address_t address_to_be_dumped;
+        
+        while (err == KERN_SUCCESS)
+        {
+            err = mach_vm_region(self, &region_start, &region_size, flavor, (vm_region_info_t) &info, &info_count, &object);
+            NSLog(@"32-bit Region Size: %llu %u, start: %llu, %llu", region_size, crypt.cryptsize, region_start, ImageInfo.load_address_);
+            
+            /*struct vm_region_basic_info {
+             vm_prot_t      protection;
+             vm_prot_t      max_protection;
+             vm_inherit_t       inheritance;
+             boolean_t      shared;
+             boolean_t      reserved;
+             uint32_t       offset; too small for a real offset
+             vm_behavior_t       behavior;
+             unsigned short      user_wired_count;
+             };*/
+            
+            struct mach_vm_info_region wow;
+            NSLog(@"PROTECTED: %u", info.protection);
+            NSLog(@"RESERVED: %d", info.reserved);
+            NSLog(@"SHARED: %d", info.shared);
+            
+            if (region_size == crypt.cryptsize)
+            {
+                NSLog(@"region_size == cryptsize");
+                address_to_be_dumped = region_start;
+                break;
+            }
+            
+            //memory_text_start = region_start;
+            region_start += region_size;
+            region_size = 0;
+            
+        }
+        
+        if (err != KERN_SUCCESS)
+        {
+            NSLog(@"failed to ASLR");
+            NSLog(@"failed: %s",  mach_error_string(err));
+            return 1;
+        }
+        
+        
+        mach_vm_offset_t storedump = (mach_vm_offset_t) malloc(crypt.cryptsize);
+        NSLog(@"memory text size %llu, file_size %llu", memory_text_size, __text.filesize);
+        
+        err = mach_vm_read_overwrite(self, region_start, region_size, &storedump, &local_size);
+        if (err != KERN_SUCCESS) {
+            NSLog(@"failed to dump");
+            NSLog(@"failed: %s",  mach_error_string(err));
+            return 1;
+        }
+        NSLog(@"success dumping, writing now");
+        //[[NSFileManager defaultManager] copyItemAtPath:framework toPath:@"framework.dump" error:nil];
+        FILE* newfile = fopen("framework.dump", "r+");
+        fwrite(&storedump, framework_cryptsize, 1, newfile);
+        
+        //fseek(newfile, off_cryptid, SEEK_SET);
+        //crypt->cryptid = 0;
+        //fwrite(&crypt, sizeof(struct encryption_info_command), 1, newfile);
+        
+        NSLog(@"wrote new cryptid, need to resign");
+        break;
+        
+        
+        break; //just test one dylib
+    }
+    
     
     if (![swappedBinaryPath isEqualToString:_originalBinary.binaryPath])
         [[NSFileManager defaultManager]removeItemAtPath:swappedBinaryPath error:nil];

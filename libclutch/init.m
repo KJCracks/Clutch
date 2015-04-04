402a3efea63fb86fecce5864612aa95b274ba1ff
ttwj <terencetanwj@gmail.com>
2015-04-04 23:04:02 +0800

    stuff

--- libclutch/init.m
+++ libclutch/init.m
@@ -7,6 +7,9 @@
 //
 
 #import "libclutch.h"
+#import <Foundation/Foundation.h>
+#import "CPDistributedMessanging.h"
+
 
 __attribute__((constructor))
 void clutchInit(int argc, const char **argv, const char **envp, const char **apple, struct ProgramVars *pvars)
@@ -20,10 +23,36 @@ void clutchInit(int argc, const char **argv, const char **envp, const char **app
 
 static void image_added(const struct mach_header *mh, intptr_t slide) {
     Dl_info image_info;
+    struct encryption_info_command* eic;
     int result = dladdr(mh, &image_info);
     
     printf("image_added %s\n",image_info.dli_fname);
     
+    NSNumber* address = [NSNumber numberWithUnsignedInteger:image_info.dli_saddr];
+    
+    struct load_command* lc;
+    
+    if (mh->magic == MH_MAGIC_64) {
+        		lc = (struct load_command *)((unsigned char *)mh + sizeof(struct mach_header_64));
+    } else {
+        		lc = (struct load_command *)((unsigned char *)mh + sizeof(struct mach_header));
+    }
+    for (int i=0; i<mh->ncmds; i++) {
+        if (lc->cmd == LC_ENCRYPTION_INFO || lc->cmd == LC_ENCRYPTION_INFO_64) {
+            eic = (struct encryption_info_command *)lc;
+    
+            NSNumber* cryptid_address = [NSNumber numberWithUnsignedInteger:(uint32_t)(mh) + eic->cryptoff];
+            
+            NSNumber* cryptsize = [NSNumber numberWithUnsignedInteger:eic->cryptsize];
+            NSNumber* cryptoff = [NSNumber numberWithUnsignedInteger:eic->cryptoff];
+        }
+    }
+    //image_info
     exit(0);
     //dumptofile(image_info.dli_fname, mh);
+}
+
+void update() {
+    
+    
 }

b2469d8cfb8292a02a9be6b174cd8dcef909c7bd
Anton Titkov <atitkov@me.com>
2015-04-06 01:15:14 +0300

    It's better to swap fat_arch. just in case + experimental LC_RPATH patch for all ClutchBundles

--- Clutch/ClutchBundle.m
+++ Clutch/ClutchBundle.m
@@ -7,6 +7,7 @@
 //
 
 #import "ClutchBundle.h"
+#import "optool.h"
 
 @interface ClutchBundle ()
 
@@ -26,6 +27,23 @@
 
 - (void)prepareForDump {
     _executable = [[Binary alloc]initWithBundle:self];
+    
+    // experimental
+    NSMutableData *data = [NSMutableData dataWithContentsOfFile:self.executable.binaryPath];
+    
+    thin_header headers[4];
+    uint32_t numHeaders = 0;
+    
+    headersFromBinary(headers, data, &numHeaders);
+    
+    for (int i = 0; i < numHeaders; i++) {
+        thin_header _thinHeader = headers[i];
+        insertRPATHIntoBinary(self.executable.binaryPath.stringByDeletingLastPathComponent, data, _thinHeader);
+    }
+    
+    [data writeToFile:self.executable.binaryPath atomically:YES];
+
+    
 }
 
 - (void)dumpToDirectoryURL:(NSURL *)directoryURL

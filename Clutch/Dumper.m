06d691351a7a8e7876c1b01daac419bb5f58067e
Andrew Udvare <audvare@gmail.com>
2016-12-24 11:38:31 -0500

    Fix all warnings

--- Clutch/Dumper.m
+++ Clutch/Dumper.m
@@ -21,11 +21,11 @@
         _thinHeader = macho;
         _originalBinary = binary;
         _shouldDisableASLR = NO;
-        
+
         _isASLRProtected = (_thinHeader.header.flags & MH_PIE);
-        
+
     }
-    
+
     return self;
 }
 
@@ -46,7 +46,7 @@
         return @"armv7s";
     else if (header.cpusubtype == CPU_SUBTYPE_ARM_V8)
         return @"armv8";
-    
+
     return @"unknown";
 }
 
@@ -65,48 +65,48 @@
 
 - (pid_t)posix_spawn:(NSString *)binaryPath disableASLR:(BOOL)yrn suspend:(BOOL) suspend
 {
-    
+
     if ((_thinHeader.header.flags & MH_PIE) && yrn) {
-        
+
         [[ClutchPrint sharedInstance] printDeveloper: @"disabling MH_PIE!!!!"];
-        
+
         _thinHeader.header.flags &= ~MH_PIE;
         [self.originalFileHandle replaceBytesInRange:NSMakeRange(_thinHeader.offset, sizeof(_thinHeader.header)) withBytes:&_thinHeader.header];
     }else if (_isASLRProtected && !yrn && !(_thinHeader.header.flags & MH_PIE)) {
-        
-        
+
+
         //thinHeader.header.flags |= MH_PIE;
         //[self.originalFileHandle replaceBytesInRange:NSMakeRange(_thinHeader.offset, sizeof(_thinHeader.header)) withBytes:&_thinHeader.header];
     }else {
         [[ClutchPrint sharedInstance] printDeveloper: @"to MH_PIE or not to MH_PIE, that is the question"];
     }
-    
-    
+
+
     pid_t pid = 0;
-    
+
     const char *path = binaryPath.UTF8String;
-    
+
     posix_spawnattr_t attr;
-    
+
     exit_with_errno (posix_spawnattr_init (&attr), "::posix_spawnattr_init (&attr) error: ");
-    
+
     short flags;
-    
+
     if (suspend) {
         flags = POSIX_SPAWN_START_SUSPENDED;
     }
     if (yrn)
         flags |= _POSIX_SPAWN_DISABLE_ASLR;
-    
+
     // Set the flags we just made into our posix spawn attributes
     exit_with_errno (posix_spawnattr_setflags (&attr, flags), "::posix_spawnattr_setflags (&attr, flags) error: ");
-    
+
     posix_spawnp (&pid, path, NULL, &attr, NULL, NULL);
-    
+
     posix_spawnattr_destroy (&attr);
-    
+
     [[ClutchPrint sharedInstance] printDeveloper:@"got the pid %u %@", pid, binaryPath];
-    
+
     return pid;
 }
 
@@ -121,61 +121,62 @@
 }
 
 -(void)swapArch {
-        
+
     [[ClutchPrint sharedInstance] printColor:ClutchPrinterColorPurple format:@"Swapping architectures.."];
-    
+
     //time to swap
     NSString* suffix = [NSString stringWithFormat:@"_%@", [Dumper readableArchFromHeader:_thinHeader]];
-    
+
     NSString *swappedBinaryPath = [_originalBinary.binaryPath stringByAppendingString:suffix];
     NSString *newSinf = [_originalBinary.sinfPath.stringByDeletingPathExtension stringByAppendingString:[suffix stringByAppendingPathExtension:_originalBinary.sinfPath.pathExtension]];
     NSString *newSupp = [_originalBinary.suppPath.stringByDeletingPathExtension stringByAppendingString:[suffix stringByAppendingPathExtension:_originalBinary.suppPath.pathExtension]];
-    
+
     NSString *newSupf;
     if ([[NSFileManager defaultManager] fileExistsAtPath:_originalBinary.supfPath]) {
         newSupf = [_originalBinary.supfPath.stringByDeletingPathExtension stringByAppendingString:[suffix stringByAppendingPathExtension:_originalBinary.supfPath.pathExtension]];
     }
-    
-    
-    
+
+
+
     NSError *error;
-    
-    if ([[NSFileManager defaultManager] fileExistsAtPath:swappedBinaryPath isDirectory:NO]){
+    BOOL isDirectory;
+
+    if ([[NSFileManager defaultManager] fileExistsAtPath:swappedBinaryPath isDirectory:&isDirectory]){
          [[NSFileManager defaultManager]removeItemAtPath:swappedBinaryPath error:nil];
     }
-   
-    
+
+
     [[NSFileManager defaultManager] copyItemAtPath:_originalBinary.binaryPath toPath:swappedBinaryPath error:&error];
-    
+
     [[ClutchPrint sharedInstance] printDeveloper: @"%@",error];
-    
+
     [[NSFileManager defaultManager] copyItemAtPath:_originalBinary.sinfPath toPath:newSinf error:nil];
-    
+
     [[NSFileManager defaultManager] copyItemAtPath:_originalBinary.suppPath toPath:newSupp error:nil];
-    
+
     if (newSupf) {
         [[NSFileManager defaultManager] copyItemAtPath:_originalBinary.supfPath toPath:newSupf error:nil];
     }
-    
+
     [self.originalFileHandle closeFile];
     self.originalFileHandle = [[NSFileHandle alloc]initWithFileDescriptor:fileno(fopen(swappedBinaryPath.UTF8String, "r+")) closeOnDealloc:YES];
-    
+
     uint32_t magic = [self.originalFileHandle intAtOffset:0];
     bool shouldSwap = magic == FAT_CIGAM;
 #define SWAP(NUM) (shouldSwap ? CFSwapInt32(NUM) : NUM)
-    
+
     NSData *buffer = [self.originalFileHandle readDataOfLength:4096];
-    
+
     struct fat_header fat = *(struct fat_header *)buffer.bytes;
     fat.nfat_arch = SWAP(fat.nfat_arch);
     int offset = sizeof(struct fat_header);
-    
+
     for (int i = 0; i < fat.nfat_arch; i++) {
         struct fat_arch arch;
         arch = *(struct fat_arch *)([buffer bytes] + offset);
-        
+
         if (!((SWAP(arch.cputype) == _thinHeader.header.cputype) && (SWAP(arch.cpusubtype) == _thinHeader.header.cpusubtype))) {
-            
+
             if (SWAP(arch.cputype) == CPU_TYPE_ARM) {
                 switch (SWAP(arch.cpusubtype)) {
                     case CPU_SUBTYPE_ARM_V6:
@@ -199,10 +200,10 @@
                         arch.cputype = SWAP(CPU_TYPE_I386);
                         arch.cpusubtype = SWAP(CPU_SUBTYPE_PENTIUM_3_XEON); //pentium 3 ftw
                         break;
-                        
+
                 }
             }else {
-                
+
                 switch (SWAP(arch.cpusubtype)) {
                     case CPU_SUBTYPE_ARM64_ALL:
                         arch.cputype = SWAP(CPU_TYPE_X86_64);
@@ -218,16 +219,16 @@
                         arch.cpusubtype = SWAP(CPU_SUBTYPE_X86_64_ALL);
                         break;
                 }
-                
+
             }
-            
+
             [self.originalFileHandle replaceBytesInRange:NSMakeRange(offset, sizeof(struct fat_arch)) withBytes:&arch];
         }
-        
+
         offset += sizeof(struct fat_arch);
     }
-    
-    
+
+
     [[ClutchPrint sharedInstance] printDeveloper: @"wrote new header to binary"];
     //we don't close file handle here since it's reused later in dumping!
 }
@@ -236,44 +237,44 @@
 {
     [[ClutchPrint sharedInstance] printDeveloper: @"checksum size %u", pages*20];
     void *checksum = malloc(pages * 20); // 160 bits for each hash (SHA1)
-    
-    
+
+
     uint32_t headerProgress = _thinHeader.header.cputype == CPU_TYPE_ARM64 ? sizeof(struct mach_header_64) : sizeof(struct mach_header);
-    
+
     uint32_t i_lcmd = 0;
     kern_return_t err;
     uint32_t pages_d = 0;
     BOOL header = TRUE;
-    
+
     uint8_t buf_d[0x1000]; // create a single page buffer
     uint8_t *buf = &buf_d[0]; // store the location of the buffer
     mach_vm_size_t local_size = 0; // amount of data moved into the buffer
-    
+
     uint32_t total = togo;
-    
+
     unsigned long percent;
-    
+
     [[ClutchPrint sharedInstance] printColor:ClutchPrinterColorPurple format:@"Dumping %@ (%@)", _originalBinary, [Dumper readableArchFromHeader:_thinHeader]];
-    
+
     while (togo > 0) {
         // get a percentage for the progress bar
-    
-    
+
+
         percent = ceil((((double)total - togo) / (double)total) * 100);
-        
+
         if ((err = mach_vm_read_overwrite(port, (mach_vm_address_t) __text_start + (pages_d * 0x1000), (vm_size_t) 0x1000, (pointer_t) buf, &local_size)) != KERN_SUCCESS)	{
-            
+
             [[ClutchPrint sharedInstance] printColor:ClutchPrinterColorPurple format:@"Failed to dump a page :("];
             free(checksum); // free checksum table
-            
+
             _kill(pid);
-            
+
             return NO;
         }
-        
-        
+
+
         if (header) {
-            
+
             // iterate over the header (or resume iteration)
             void *curloc = buf + headerProgress;
             for (;i_lcmd<_thinHeader.header.ncmds;i_lcmd++) {
@@ -287,7 +288,7 @@
                 } else {
                     lcmd_size = l_cmd->cmdsize;
                 }
-                
+
                 if (l_cmd->cmd == LC_ENCRYPTION_INFO) {
                     struct encryption_info_command *newcrypt = (struct encryption_info_command *) curloc;
                     newcrypt->cryptid = 0; // change the cryptid to 0
@@ -297,7 +298,7 @@
                     newcrypt->cryptid = 0; // change the cryptid to 0
                     [[ClutchPrint sharedInstance] printColor:ClutchPrinterColorPurple format:@"Patched cryptid (64bit segment)"];
                 }
-                
+
                 curloc += lcmd_size;
                 if ((void *)curloc >= (void *)buf + 0x1000) {
                     // we are currently extended past the header page
@@ -307,31 +308,31 @@
                     goto writedata;
                 }
             }
-            
+
             header = FALSE;
         }
-        
+
     writedata:
         [fileHandle writeData:[NSData dataWithBytes:buf length:0x1000]]; //write the page
         sha1(checksum + (20 * pages_d), buf, 0x1000); // perform checksum on the page
         togo -= 0x1000; // remove a page from the togo
         pages_d += 1; // increase the amount of completed pages
     }
-    
-    
+
+
     //nice! now let's write the new checksum data
     [[ClutchPrint sharedInstance] printColor:ClutchPrinterColorPurple format:@"Writing new checksum"];
     [fileHandle seekToFileOffset:(begin + hashOffset)];
-    
+
 
     NSData* trimmed_checksum = [[NSData dataWithBytes:checksum length:pages*20] subdataWithRange:NSMakeRange(0, 20*pages_d)];
     free(checksum);
     [fileHandle writeData:trimmed_checksum];
-    
-    
+
+
     [[ClutchPrint sharedInstance] printDeveloper: @"Done writing checksum"];
-    
-    
+
+
     return YES;
 }
 
@@ -353,28 +354,28 @@ void *safe_trim(void *p, size_t n) {
     [[ClutchPrint sharedInstance] printDeveloper: @"Segment cputype: %u, cpusubtype: %u", _thinHeader.header.cputype, _thinHeader.header.cpusubtype];
     [[ClutchPrint sharedInstance] printDeveloper: @"Device cputype: %u, cpusubtype: %u", Device.cpu_type, Device.cpu_subtype];
     [[ClutchPrint sharedInstance] printDeveloper: @"Dumper supports cputype %u", self.supportedCPUType];
-    
+
 
     if (self.supportedCPUType != _thinHeader.header.cputype) {
         [[ClutchPrint sharedInstance] printDeveloper:@"Dumper <%@> does not support the %@ architecture", NSStringFromClass([self class]), [Dumper readableArchFromHeader:_thinHeader]];
         return ArchCompatibilityNotCompatible;
     }
-    
+
     else if ((Device.cpu_type == CPU_TYPE_ARM64) && (_thinHeader.header.cputype == CPU_TYPE_ARM)) {
         [[ClutchPrint sharedInstance] printDeveloper: @"God Mode On"];
         return ArchCompatibilityCompatible;
     }
-    
+
     else if ((_thinHeader.header.cpusubtype > Device.cpu_subtype) || (_thinHeader.header.cputype > Device.cpu_type)) {
         [[ClutchPrint sharedInstance] printDeveloper: @"Cannot use dumper %@, device not supported", NSStringFromClass([self class])];
         return ArchCompatibilityNotCompatible;
     }
-    
 
-   
-    
+
+
+
     return ArchCompatibilityCompatible;
-    
+
 }
 
 @end
--- Clutch/FrameworkDumper.m
+++ Clutch/FrameworkDumper.m
@@ -20,74 +20,74 @@
 
 - (BOOL)dumpBinary
 {
-    
+
     ClutchBundle *bundle = [_originalBinary valueForKey:@"_bundle"];
-    
+
     NSString *binaryDumpPath = [_originalBinary.workingPath stringByAppendingPathComponent:_originalBinary.binaryPath.lastPathComponent];
 
-    
+
     NSString* swappedBinaryPath = _originalBinary.binaryPath, *newSinf = _originalBinary.sinfPath, *newSupp = _originalBinary.suppPath, *newSupf = _originalBinary.supfPath; // default values if we dont need to swap archs
-    
+
     //check if cpusubtype matches
     if ((_thinHeader.header.cpusubtype != [Device cpu_subtype]) && (_originalBinary.hasMultipleARMSlices || (_originalBinary.hasARM64Slice && ([Device cpu_type]==CPU_TYPE_ARM64)))) {
-        
+
         NSString* suffix = [NSString stringWithFormat:@"_%@", [Dumper readableArchFromHeader:_thinHeader]];
-        
+
         swappedBinaryPath = [_originalBinary.binaryPath stringByAppendingString:suffix];
         newSinf = [_originalBinary.sinfPath.stringByDeletingPathExtension stringByAppendingString:[suffix stringByAppendingPathExtension:_originalBinary.sinfPath.pathExtension]];
         newSupp = [_originalBinary.suppPath.stringByDeletingPathExtension stringByAppendingString:[suffix stringByAppendingPathExtension:_originalBinary.suppPath.pathExtension]];
         newSupf = [_originalBinary.supfPath.stringByDeletingPathExtension stringByAppendingString:[suffix stringByAppendingPathExtension:_originalBinary.supfPath.pathExtension]];
 
-        
+
         [self swapArch];
-        
+
     }
-        
+
     NSFileHandle *newFileHandle = [[NSFileHandle alloc]initWithFileDescriptor:fileno(fopen(binaryDumpPath.UTF8String, "r+"))];
-    
+
     [newFileHandle seekToFileOffset:_thinHeader.offset + _thinHeader.size];
-    
+
     struct linkedit_data_command ldid; // LC_CODE_SIGNATURE load header (for resign)
     struct encryption_info_command crypt; // LC_ENCRYPTION_INFO load header (for crypt*)
     struct segment_command __text; // __TEXT segment
-    
+
     struct super_blob *codesignblob; // codesign blob pointer
     struct code_directory directory; // codesign directory index
-    
+
     BOOL foundCrypt = NO, foundSignature = NO, foundStartText = NO;
-    
+
     uint64_t __text_start = 0;
-    
+
     [[ClutchPrint sharedInstance] printDeveloper: @"32bit dumping: arch %@ offset %u", [Dumper readableArchFromHeader:_thinHeader], _thinHeader.offset];
     uint32_t cryptlc_offset;
-    
+
     for (int i = 0; i < _thinHeader.header.ncmds; i++) {
-        
+
         uint32_t cmd = [newFileHandle intAtOffset:newFileHandle.offsetInFile];
         uint32_t size = [newFileHandle intAtOffset:newFileHandle.offsetInFile+sizeof(uint32_t)];
-        
+
         switch (cmd) {
             case LC_CODE_SIGNATURE: {
                 [newFileHandle getBytes:&ldid inRange:NSMakeRange(newFileHandle.offsetInFile,sizeof(struct linkedit_data_command))];
                 foundSignature = YES;
-                
+
                 [[ClutchPrint sharedInstance] printDeveloper: @"FOUND CODE SIGNATURE: dataoff %u | datasize %u",ldid.dataoff,ldid.datasize];
-                
+
                 break;
             }
             case LC_ENCRYPTION_INFO: {
                 cryptlc_offset = newFileHandle.offsetInFile;
                 [newFileHandle getBytes:&crypt inRange:NSMakeRange(newFileHandle.offsetInFile,sizeof(struct encryption_info_command))];
                 foundCrypt = YES;
-                
+
                 [[ClutchPrint sharedInstance] printDeveloper: @"FOUND ENCRYPTION INFO: cryptoff %u | cryptsize %u | cryptid %u",crypt.cryptoff,crypt.cryptsize,crypt.cryptid];
-                
+
                 break;
             }
             case LC_SEGMENT:
             {
                 [newFileHandle getBytes:&__text inRange:NSMakeRange(newFileHandle.offsetInFile,sizeof(struct segment_command))];
-                
+
                 if (strncmp(__text.segname, "__TEXT", 6) == 0) {
                     foundStartText = YES;
                     [[ClutchPrint sharedInstance] printDeveloper: @"FOUND %s SEGMENT",__text.segname];
@@ -96,36 +96,36 @@
                 break;
             }
         }
-        
+
         [newFileHandle seekToFileOffset:newFileHandle.offsetInFile + size];
-        
+
         if (foundCrypt && foundSignature && foundStartText)
             break;
     }
-    
+
     // we need to have all of these
     if (!foundCrypt || !foundSignature || !foundStartText) {
         [[ClutchPrint sharedInstance] printDeveloper: @"dumping binary: some load commands were not found %@ %@ %@",foundCrypt?@"YES":@"NO",foundSignature?@"YES":@"NO",foundStartText?@"YES":@"NO"];
         return NO;
     }
-    
+
     [[ClutchPrint sharedInstance] printDeveloper: @"starting to ldid"];
-    
+
     NSUInteger begin;
-    
+
     //seek to ldid offset
-    
+
     codesignblob = malloc(ldid.datasize);
-    
+
     [newFileHandle seekToFileOffset:_thinHeader.offset + ldid.dataoff];
     [newFileHandle getBytes:codesignblob inRange:NSMakeRange(newFileHandle.offsetInFile, ldid.datasize)];
-    
+
     [[ClutchPrint sharedInstance] printDeveloper: @"hello it's me"];
-    
+
     uint32_t countBlobs = CFSwapInt32(codesignblob->count); // how many indexes?
-    
-    
-    
+
+
+
     for (uint32_t index = 0; index < countBlobs; index++) { // is this the code directory?
         if (CFSwapInt32(codesignblob->index[index].type) == CSSLOT_CODEDIRECTORY) {
             // we'll find the hash metadata in here
@@ -137,50 +137,50 @@
         }
     }
     free(codesignblob);
-    
+
     uint32_t pages = CFSwapInt32(directory.nCodeSlots); // get the amount of codeslots
-    
+
     if (pages == 0) {
         [[ClutchPrint sharedInstance] printColor:ClutchPrinterColorPurple format:@"pages == 0"];
         return NO;
     }
-    
+
     [newFileHandle closeFile];
-    
-    
+
+
     [[ClutchPrint sharedInstance] printDeveloper: @"hello from the other side"];
-    
+
     extern char **environ;
     posix_spawnattr_t attr;
-    
+
     pid_t pid;
-    
-   
-    
+
+
+
     NSUUID* workingUUID = [NSUUID new];
     NSString* workingPath = [NSTemporaryDirectory() stringByAppendingPathComponent:[@"clutch" stringByAppendingPathComponent:workingUUID.UUIDString]];
-    
+
     while ([[NSFileManager defaultManager] fileExistsAtPath:workingPath]) {
         workingUUID = [NSUUID new];
         workingPath = [NSTemporaryDirectory() stringByAppendingPathComponent:[@"clutch" stringByAppendingPathComponent:workingUUID.UUIDString]];
     }
-    
+
     [[NSFileManager defaultManager] createDirectoryAtPath:workingPath withIntermediateDirectories:YES attributes:nil error:nil];
 
-    
-    
+
+
     if (![[NSFileManager defaultManager] copyItemAtPath:[NSProcessInfo processInfo].arguments[0] toPath:[workingPath stringByAppendingPathComponent:@"clutch"] error:nil]) {
         [[ClutchPrint sharedInstance] printError:@"Failed to copy clutch to %@", workingPath];
         return NO;
     }
-    
+
     if (_originalBinary.frameworksPath == nil) {
         [[ClutchPrint sharedInstance] printError:@"Could not find Frameworks path to create symbolic link to"];
         return NO;
     }
-    
+
     [[NSFileManager defaultManager] createSymbolicLinkAtPath:[workingPath stringByAppendingPathComponent:@"Frameworks"] withDestinationPath:_originalBinary.frameworksPath error:nil];
-    
+
     const char *argv[] = {[[workingPath stringByAppendingPathComponent:@"clutch"] UTF8String],
         "-f",
         swappedBinaryPath.UTF8String,
@@ -195,34 +195,34 @@
         [NSString stringWithFormat:@"%u", crypt.cryptsize].UTF8String,
         [NSString stringWithFormat:@"%u", cryptlc_offset].UTF8String,
         NULL};
-    
+
     [[ClutchPrint sharedInstance] printDeveloper: @"i must have called a thousand times!"];
-        
+
     [[ClutchPrint sharedInstance] printDeveloper: @"hello potato posix_spawn %@", [[NSString alloc] initWithUTF8String:argv[0]]];
 
-    
+
     posix_spawnattr_init (&attr);
-    
+
     size_t ocount = 0;
-    
+
     cpu_type_t cpu_type = CPU_TYPE_ARM;
-    
-    
+
+
     posix_spawnattr_setbinpref_np (&attr, 1, &cpu_type, &ocount);
-    
-    
+
+
     short flags = POSIX_SPAWN_START_SUSPENDED;
     // Set the flags we just made into our posix spawn attributes
     exit_with_errno (posix_spawnattr_setflags (&attr, flags), "::posix_spawnattr_setflags (&attr, flags) error: ");
-    
+
     int dumpResult = posix_spawnp(&pid, argv[0], NULL, &attr, (char* const*)argv, environ);
-    __block finalDumpResult = 9999; //it shouldn't be 9999
-    
+    __block NSUInteger finalDumpResult = 9999; //it shouldn't be 9999
+
     if (dumpResult == 0) {
         [[ClutchPrint sharedInstance] printDeveloper: @"Child pid: %i", pid];
-        
+
         dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
-        
+
         dispatch_sync(queue, ^{
             kill(pid, SIGCONT);
 			int dumpResult = 0;
@@ -233,26 +233,26 @@
                 perror("waitpid");
             }
         });
-                      
+
     } else {
         [[ClutchPrint sharedInstance] printDeveloper: @"posix_spawn: %s", strerror(dumpResult)];
     }
-    
-    
+
+
     if (![swappedBinaryPath isEqualToString:_originalBinary.binaryPath])
         [[NSFileManager defaultManager]removeItemAtPath:swappedBinaryPath error:nil];
     if (![newSinf isEqualToString:_originalBinary.sinfPath])
         [[NSFileManager defaultManager]removeItemAtPath:newSinf error:nil];
     if (![newSupp isEqualToString:_originalBinary.suppPath])
         [[NSFileManager defaultManager]removeItemAtPath:newSupp error:nil];
-    
+
     [[NSFileManager defaultManager] removeItemAtPath:workingPath error:nil];
-    
-   
-    
+
+
+
     if (finalDumpResult == 0)
         return YES;
-    
+
     return NO;
 }
 

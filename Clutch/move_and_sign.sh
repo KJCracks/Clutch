6eebfe08e749a5f4d951c43fd776f2ee5895a599
Andrew Udvare <audvare@gmail.com>
2017-01-31 07:26:24 -0500

    Minor change to signing script

--- Clutch/move_and_sign.sh
+++ Clutch/move_and_sign.sh
@@ -1,15 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-#  move_and_sign.sh
-#  Clutch
-#
-#  Created by dev on 10/01/2017.
-#
-
-if [ ! -d build ]; then
-    mkdir "build"
-fi
-
+cd "$PROJECT_DIR"
+! [ -d build ] && mkdir build
 cp "$BUILT_PRODUCTS_DIR/$EXECUTABLE_PATH" "build/"
-
 codesign -fs- --entitlements "$CODE_SIGN_ENTITLEMENTS" "build/$EXECUTABLE_NAME"

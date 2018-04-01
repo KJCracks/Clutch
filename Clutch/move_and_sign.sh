13cf43fd2060af890a6f92f0d1cb8d4cac9ca7dd
Andrew Udvare <audvare@gmail.com>
2018-04-01 05:09:34 -0400

    Fix signing issue for latest Xcode version

--- Clutch/move_and_sign.sh
+++ Clutch/move_and_sign.sh
@@ -3,4 +3,4 @@
 cd "$PROJECT_DIR"
 ! [ -d build ] && mkdir build
 cp "$BUILT_PRODUCTS_DIR/$EXECUTABLE_PATH" "build/"
-codesign -fs- --entitlements "$CODE_SIGN_ENTITLEMENTS" "build/$EXECUTABLE_NAME"
+codesign -fs- --entitlements 'Clutch/Clutch.entitlements' "build/$EXECUTABLE_NAME"

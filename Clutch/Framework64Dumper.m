ba58ed10b2b923b0ea0be0043078449cf4a63db6
Saagar Jha <saagar@saagarjha.com>
2023-11-01 16:03:11 -0700

    Set DYLD_FRAMEWORK_PATH when dumping frameworks
    This fixes most issues with frameworks that refer to each other.

--- Clutch/Framework64Dumper.m
+++ Clutch/Framework64Dumper.m
@@ -154,6 +154,11 @@
     }
 
     [newFileHandle closeFile];
+	
+	// Lazy, but when dumping a framework it may refer to other frameworks by
+	// rpath. Set our own framework path to the app's framework directory so
+	// that these dependent loads will fallback here.
+	setenv("DYLD_FRAMEWORK_PATH", self.originalBinary.frameworksPath.UTF8String, 0);
 
     extern char **environ;
     posix_spawnattr_t attr;
